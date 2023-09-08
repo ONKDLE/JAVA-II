1534e9761b67012ada96f63a24551c1676663151
LJG <129822754+ONKDLE@users.noreply.github.com>
2023-09-08 11:11:26 +0900

    Add files via upload

---
+++ src/myAnimalTest.java
@@ -0,0 +1,44 @@
+/*
+ * 작성일 : 2023년 9월 8일
+ * 작성자 : 컴퓨터 공학부 202295029 이종국
+ * 설명 : 추상클래스 - 동물의 울음소리를 출력하자.
+ */
+// 추상 클래스 - 동물
+abstract class myAnimal {
+	//추상 메소드 : 동물의 소리를 반환하는 메소드
+	abstract String sound();
+	
+	//일반 메소드 : 동물의 기본 정보 출력하는 메소드
+	void displayInfo() {
+		System.out.println("동물의 울음소리 입니다.");
+	}
+}
+
+//고양이 클래스 - 추상 클래스 상속
+//고양이 울음소리 반환
+class myCat extends myAnimal {
+	@Override
+	String sound() {
+		// 재정의.
+		return "야옹~~";
+	}
+	
+	
+
+	@Override
+	void displayInfo() {
+		// TODO Auto-generated method stub
+		System.out.println("고양이 울음소리 입니다.");
+	}
+	
+}
+public class myAnimalTest {
+	public static void main(String[] args) {
+		// TODO AUTE-generated method stub
+		myAnimal cat = new myCat();
+		
+		cat.displayInfo();
+		System.out.println(cat.sound());
+	}
+
+}
